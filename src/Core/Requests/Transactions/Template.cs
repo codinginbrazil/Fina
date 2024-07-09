1cd5999861fea148c388546b5eb6f585ebca2458
sswellington <sswellington@pm.me>
2024-07-08 21:08:47 -0300

    refactor: transactions

---
+++ src/Core/Requests/Transactions/Template.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Requests.Transactions;
+
+public abstract class Template : Request
+{
+    [Required(ErrorMessage = "Valor inválido")]
+    public decimal Amount { get; set; }
+
+    [Required(ErrorMessage = "Categoria inválida")]
+    public long CategoryId { get; set; }
+
+    [Required(ErrorMessage = "Data inválido")]
+    public DateTime? PaidOrReceivedAt { get; set; }
+}
--- src/Core/Requests/Transactions/Update.cs
+++ src/Core/Requests/Transactions/Update.cs
@@ -1,15 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Core.Requests.Transactions;
 
-public sealed class Update : Request
-{
-    [Required(ErrorMessage = "Valor inválido")]
-    public decimal Amount { get; set; }
-
-    [Required(ErrorMessage = "Categoria inválida")]
-    public long CategoryId { get; set; }
-
-    [Required(ErrorMessage = "Data inválido")]
-    public DateTime? PaidOrReceivedAt { get; set; }
-}
+public sealed class Update : Template { }
