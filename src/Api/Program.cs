4f755e77009f8be2af9ddfa6a50a7b99b87e3872
sswellington <sswellington@pm.me>
2024-07-11 19:47:39 -0300

    env: dapper and npgsql

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,11 @@
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddScoped<IDbConnection>((sp) => new Npgsql.NpgsqlConnection(connectionString));
+
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
-
 app.Run();
